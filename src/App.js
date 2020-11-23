ad678e5df7193b32273603bed2e1a7f9ebe45dba
sarahrourke <serourke@gmail.com>
2020-11-23 09:45:08 -0600

    moved 'import bootstrap...css' to app.js from index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Items from './components/ItemList';
 import Item from './components/Item';
