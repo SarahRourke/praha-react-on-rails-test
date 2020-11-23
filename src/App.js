92b085bd6b51e64094c2b80d880ab0f6f5c28e1f
sarahrourke <serourke@gmail.com>
2020-11-23 09:42:28 -0600

    removed unused variables for browserrouter and router from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Items from './components/ItemList';
 import Item from './components/Item';
@@ -16,23 +16,22 @@ function App(props) {
     
       <div className="App">
         <Header />
+        
         <div className="main-container">
-        <Link to="/create">Add Item</Link>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/items" component={Items}/>
+       
+          <Switch>
+            <Route exact path="/" component=    {Home}/>
+            <Route exact path="/items" component={Items}/>
+            <Route exact path="/items/:id" component={Item} />
+            <Route exact path="/create" component={AddItemForm} />
+            <Route exact path="/update" component={EditItemForm} />
             
-          
-          <Route exact path="/items/:id" component={Item} />
-          <Route exact path="/create" component={AddItemForm} />
-          <Route exact path="/update" component={EditItemForm} />
-          
-          
             
-          
-          
-        </Switch>  
-      
+              
+            
+            
+          </Switch>  
+        
         </div>
       </div>
     
