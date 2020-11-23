a87f60ed4a464597e21aa52925960485a26ce748
sarahrourke <serourke@gmail.com>
2020-11-23 09:50:28 -0600

    cleaned up code format in app.ja

--- src/App.js
+++ src/App.js
@@ -11,31 +11,19 @@ import AddItemForm from './components/AddItemForm';
 
 
 function App(props) {
-
-  
   return (
-    
       <div className="App">
         <Header />
-        
-        <div className="main-container">
-       
-          <Switch>
-            <Route exact path="/" component=    {Home}/>
-            <Route exact path="/items" component={Items}/>
-            <Route exact path="/items/:id" component={Item} />
-            <Route exact path="/create" component={AddItemForm} />
-            <Route exact path="/update" component={EditItemForm} />
-            
-            
-              
-            
-            
-          </Switch>  
-        
-        </div>
+          <div className="main-container">
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/items" component={Items}/>
+                <Route exact path="/items/:id" component={Item} />
+                <Route exact path="/create" component={AddItemForm} />
+                <Route exact path="/update" component={EditItemForm} />
+            </Switch>  
+          </div>
       </div>
-    
   );
 }
 
